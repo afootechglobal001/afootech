11a64dd6bafc077b92036422c72d91fca4332f68
Emmytech12 <seunemmanuel107@gmail.com>
2026-06-23 21:14:28 +0100

    update: update 1 @23-06-2026 - update on student siwes regsitration

--- training/config/content-page.php
+++ training/config/content-page.php
@@ -1,4 +1,8 @@
 <?php if ($page == 'registerPage') { ?>
+    <script>
+        studentSession = JSON.parse(localStorage.getItem("studentCompleteBioDataSession")) || {};
+    </script>
+
     <div class="form-div" data-aos="fade-in" data-aos-duration="1200">
         <h1>Welcome <span>Student!</span></h1>
         <p>
@@ -11,7 +15,8 @@
                     <script>
                         textField({
                             id: 'firstName',
-                            title: 'First Name'
+                            title: 'First Name',
+                            value: studentSession?.firstName ?? ''
                         });
                     </script>
                 </div>
@@ -20,7 +25,8 @@
                     <script>
                         textField({
                             id: 'lastName',
-                            title: 'Last Name'
+                            title: 'Last Name',
+                            value: studentSession?.lastName ?? ''
                         });
                     </script>
                 </div>
@@ -31,7 +37,8 @@
                     textField({
                         id: 'emailAddress',
                         title: 'Email Address',
-                        type: 'email'
+                        type: 'email',
+                        value: studentSession?.emailAddress ?? ''
                     });
                 </script>
             </div>
@@ -40,7 +47,8 @@
                 <script>
                     textField({
                         id: 'phoneNumber',
-                        title: 'Phone Number'
+                        title: 'Phone Number',
+                        value: studentSession?.phoneNumber ?? ''
                     });
                 </script>
             </div>
@@ -52,10 +60,13 @@
             </div>
         </div>
     </div>
-    <script>_setInputValues();</script>
 <?php } ?>
 
 <?php if ($page == 'institutionDetailsPage') { ?>
+    <script>
+        studentSession = JSON.parse(localStorage.getItem("studentCompleteBioDataSession")) || {};
+    </script>
+
     <div class="form-div">
         <h1>Institution <span>Information</span></h1>
         <p>
@@ -63,22 +74,24 @@
         </p>
 
         <div class="inner-form">
-            <div class="text_field_container" id="institutionType_container">
+            <div class="text_field_container" id="institutionTypeId_container">
                 <script>
                     selectField({
-                        id: 'institutionType',
-                        title: 'Select Institution Type'
+                        id: 'institutionTypeId',
+                        title: 'Select Institution Type',
+                        fieldValue: studentSession?.institutionTypeId ?? '',
+                        fieldLabel: studentSession?.institutionTypeName ?? ''
                     });
+                    _getSelectInstitutionType('institutionTypeId');
                 </script>
             </div>
-
-
 
             <div class="text_field_container" id="nameOfInstitution_container">
                 <script>
                     textField({
                         id: 'nameOfInstitution',
-                        title: 'Name Of Institution'
+                        title: 'Name Of Institution',
+                        value: studentSession?.nameOfInstitution ?? ''
                     });
                 </script>
             </div>
@@ -87,7 +100,8 @@
                 <script>
                     textField({
                         id: 'department',
-                        title: 'Department'
+                        title: 'Department',
+                        value: studentSession?.department ?? ''
                     });
                 </script>
             </div>
@@ -96,7 +110,9 @@
                 <script>
                     selectField({
                         id: 'levelId',
-                        title: 'Select Cuurent Level'
+                        title: 'Select Current Level',
+                        fieldValue: studentSession?.levelId ?? '',
+                        fieldLabel: studentSession?.levelName ?? ''
                     });
                 </script>
             </div>
@@ -106,6 +122,7 @@
                     textField({
                         id: 'matricNo',
                         title: 'Refrence/Matric No',
+                        value: studentSession?.matricNo ?? ''
                     });
                 </script>
             </div>
@@ -114,7 +131,7 @@
                 <button class="btn back-btn" title="Back to previous" onclick="_getNextPage({page: 'registerPage'});"><i
                         class="bi bi-arrow-left-circle"></i> Back</button>
                 <button class="btn" id="submitBtn" title="Proceed"
-                    onclick="_getNextPage({page: 'programDetailsPage'});">Proceed <i
+                    onclick="_proceedInstitutionInformation();">Proceed <i
                         class="bi bi-arrow-right-circle"></i></button>
             </div>
         </div>
@@ -122,6 +139,10 @@
 <?php } ?>
 
 <?php if ($page == 'programDetailsPage') { ?>
+    <script>
+        getSelectedProgramCourseDurationSession = JSON.parse(localStorage.getItem("getSelectedProgramCourseDurationSession")) || {};
+    </script>
+
     <div class="form-div" data-aos="fade-in" data-aos-duration="1200">
         <h1>Program <span>Information</span></h1>
         <p>
@@ -133,8 +154,11 @@
                 <script>
                     selectField({
                         id: 'programId',
-                        title: 'Slect Program'
+                        title: 'Select Program',
+                        fieldValue: getSelectedProgramCourseDurationSession?.programData?.[0]?.programId || '',
+                        fieldLabel: getSelectedProgramCourseDurationSession?.programData?.[0]?.programName || ''
                     });
+                    _getSelectPrograms('programId');
                 </script>
             </div>
 
@@ -142,16 +166,27 @@
                 <script>
                     selectField({
                         id: 'courseId',
-                        title: 'Select Course'
+                        title: 'Select Course',
+                        fieldValue: getSelectedProgramCourseDurationSession?.courseData?.[0]?.courseId ?? '',
+                        fieldLabel: getSelectedProgramCourseDurationSession?.courseData?.[0]?.courseName ?? ''
                     });
+                    _getSelectCourse('courseId');
                 </script>
             </div>
 
-            <div class="text_field_container" id="duration_container">
+            <div class="text_field_container" id="durationId_container">
                 <script>
-                    selectField({
-                        id: 'duration',
-                        title: 'Select Duration',
+                    $(document).ready(function() {
+                        const selectedDuration = getSelectedProgramCourseDurationSession?.data?.find(
+                            item => item.durationId == getSelectedProgramCourseDurationSession?.data?.[0]?.durationId
+                        );
+
+                        selectField({
+                            id: 'durationId',
+                            title: 'Select Duration',
+                            fieldValue: selectedDuration?.durationId ?? '',
+                            fieldLabel: (selectedDuration?.durationName ?? '') + ' (<s>N</s>' + thousandSeperator(selectedDuration?.trainingAmount ?? 0) +')'
+                        });
                     });
                 </script>
             </div>
@@ -160,7 +195,7 @@
                 <button class="btn back-btn" title="Back to previous"
                     onclick="_getNextPage({page: 'institutionDetailsPage'});"><i class="bi bi-arrow-left-circle"></i>
                     Back</button>
-                <button class="btn" id="submitBtn" title="Proceed" onclick="_getNextPage({page: 'passportPage'});">Proceed
+                <button class="btn" id="submitBtn" title="Proceed" onclick="_proceedProgramDetailsData();">Proceed
                     <i class="bi bi-arrow-right-circle"></i></button>
             </div>
         </div>
@@ -168,6 +203,10 @@
 <?php } ?>
 
 <?php if ($page == 'passportPage') { ?>
+    <script>
+        studentSession = JSON.parse(localStorage.getItem("studentCompleteBioDataSession")) || {};
+    </script>
+
     <div class="form-div" data-aos="fade-in" data-aos-duration="1200">
         <h1>Upload <span>Passport</span></h1>
         <p>
@@ -175,7 +214,7 @@
         </p>
 
         <div class="inner-form">
-            <div class="cam-pix">
+            <div class="cam-pix" id="issueBorder">
                 <div class="btn-container">
                     <button class="capture-btn" title="Take Picture" onClick="takeSnapShot()"> <i
                             class="bi-camera-fill"></i> TAKE
@@ -191,23 +230,150 @@
                         BROWSE IMAGE
                     </button>
                 </div>
-                <div class="cam-pix-inner" id="cam-pix">
-                    <img src="<?php echo $websiteUrl ?>/all-images/images/sample.jpg" />
-                </div>
-            </div>
-            <div class="issue-text" id="issues_blogPix"></div>
 
-            <div class="alert alert-success form-alert-div">
-                <span><i class="bi-pencil-square"></i> Note:</span>
-                Kindly note that a registration fee of (<strong><span style="font-size:14px;"
-                        id=""><s>N</s>10,000.00</span></strong>) will be required before completing your registration.
+                <div class="cam-pix-inner" id="cam-pix">
+                    <img src="<?php echo $websiteUrl ?>/uploaded_files/passport/sample.jpg" />
+                </div>
+
+                <script>
+                    $(document).ready(function() {
+                        const passport = studentSession?.passport ? studentSession?.passport : "<?php echo $websiteUrl ?>/uploaded_files/passport/sample.jpg";
+                        $("#cam-pix img").attr("src", passport).attr("alt", "Profile Image");
+                    });
+                </script>
             </div>
+            <div class="issue-text" id="issues_passport"></div>
+            
             <div class="btn-div">
                 <button class="btn back-btn" title="Back to previous"
                     onclick="_getNextPage({page: 'programDetailsPage'});"><i class="bi bi-arrow-left-circle"></i>
                     Back</button>
+                <button class="btn" id="submitBtn" title="Proceed" onclick="_proceedPassportData();">Proceed
+                    <i class="bi bi-arrow-right-circle"></i></button>
+            </div>
+        </div>
+    </div>
+<?php } ?>
+
+<?php if ($page == 'summaryPage') { ?>
+    <script>
+        studentSession = JSON.parse(localStorage.getItem("studentCompleteBioDataSession")) || {};
+        getSelectedProgramCourseDurationSession = JSON.parse(localStorage.getItem("getSelectedProgramCourseDurationSession")) || {};
+    </script>
+
+    <div class="form-div" data-aos="fade-in" data-aos-duration="1200">
+        <h1>Registration <span>Review</span></h1>
+        <p>
+            Kindly review your submitted information to ensure everything is accurate before completing your SIWES registration.
+        </p>
+
+        <div class="inner-form">
+            <div class="alert alert-success form-alert-div">
+                <div class="alert-list-div">
+                    <div class="alert-list-back-div">
+                        <div class="alert-list">
+                            <div>FullName:</div>
+                            <div>
+                                <span id="fullName">
+                                    <script>
+                                        $("#fullName").html(studentSession?.firstName + ' ' + studentSession?.lastName);
+                                    </script>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="alert-list-back-div">
+                        <div class="alert-list">
+                            <div>Email Address:</div>
+                            <div>
+                                <span id="emailAddress">
+                                    <script>
+                                        $("#emailAddress").html(studentSession?.emailAddress);
+                                    </script>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="alert-list-back-div">
+                        <div class="alert-list">
+                            <div>Phone Number:</div>
+                            <div>
+                                <span id="phoneNumber">
+                                    <script>
+                                        $("#phoneNumber").html(studentSession?.phoneNumber);
+                                    </script>
+                                </span>
+                            </div>
+                        </div>
+                    </div>    
+                    
+                    <div class="alert-list-back-div">
+                        <div class="alert-list">
+                            <div>Program:</div>
+                            <div>
+                                <span id="programName">
+                                    <script>
+                                        $("#programName").html(getSelectedProgramCourseDurationSession?.programData?.[0]?.programName);
+                                    </script>
+                                </span>
+                            </div>
+                        </div>
+                    </div> 
+
+                    <div class="alert-list-back-div">
+                        <div class="alert-list">
+                            <div>Course:</div>
+                            <div>
+                                <span id="courseName">
+                                    <script>
+                                        $("#courseName").html(getSelectedProgramCourseDurationSession?.courseData?.[0]?.courseName);
+                                    </script>
+                                </span>
+                            </div>
+                        </div>
+                    </div> 
+
+                    <div class="alert-list-back-div">
+                        <div class="alert-list">
+                            <div>Duration:</div>
+                            <div>
+                                <span id="durationName">
+                                    <script>
+                                        $("#durationName").html(getSelectedProgramCourseDurationSession?.data?.[0]?.durationName);
+                                    </script>
+                                </span>
+                            </div>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+
+            <div class="alert alert-success form-alert-div">
+                <span><i class="bi-pencil-square"></i> Note:</span>
+                Kindly note that a registration fee of (<strong><span style="font-size:14px;" id="trainingAmount"><s>N</s>
+                    <script>
+                        $("#trainingAmount").html('<s>N</s>' + thousandSeperator(getSelectedProgramCourseDurationSession?.data?.[0]?.trainingAmount));
+                    </script></span></strong>) will be required before completing your registration.
+            </div>
+
+            <div class="text_field_container" id="paymentMethodId_container">
+                <script>
+                selectField({
+                    id: 'paymentMethodId',
+                    title: 'Select Payment Method'
+                });
+                _getSelectPaymentMethod('paymentMethodId');
+                </script>
+            </div>
+
+            <div class="btn-div">
+                <button class="btn back-btn" title="Back to previous"
+                    onclick="_getNextPage({page: 'passportPage'});"><i class="bi bi-arrow-left-circle"></i>
+                    Back</button>
                 <button class="btn full-btn" id="submitBtn" title="Proceed to Payment"
-                    onclick="_completeRegistration();">Proceed to Payment <i class="bi bi-credit-card"></i></button>
+                    onclick="_proceedToPayment();">Proceed to Payment <i class="bi bi-credit-card"></i></button>
             </div>
         </div>
     </div>
